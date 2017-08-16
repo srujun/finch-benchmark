d227f66e19636e33987cb3c14211dead645f3f0d
Srujun Thanmay Gupta <theununtrium@gmail.com>
2017-08-15 22:22:44 -0500

    Added Vagrant files

---
+++ provision.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt install -y python3-pip
+sudo -H pip3 install --upgrade pip setuptools wheel
+sudo apt install -y default-jdk
+sudo echo "JAVA_HOME=\"/usr/lib/jvm/java-8-openjdk-amd64\"" >> /etc/environment
