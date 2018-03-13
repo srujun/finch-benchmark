b23048dd456e1495925da4a83c1114beb67ba740
Srujun Thanmay Gupta <theununtrium@gmail.com>
2018-03-13 11:30:04 -0500

    Small Python code fixes and increase container count

--- feed2influx.py
+++ feed2influx.py
@@ -2,14 +2,20 @@ import datetime as dt
 import json
 from numbers import Number
 from pprint import pprint
+import sys
 
 from kafka import KafkaConsumer
 from influxdb import InfluxDBClient
 
-INFLUX_IP = '192.168.1.4'
+INFLUX_IP = 'localhost'
 INFLUX_PORT = 8086
 
 def main():
+
+    if len(sys.args) > 1:
+        INFLUX_IP = sys.args[1]
+        INFLUX_PORT = int(sys.args[2])
+
     consumer = KafkaConsumer(
         'metrics', value_deserializer=lambda m: json.loads(m.decode('ascii')),
         bootstrap_servers=['localhost:9092'],
@@ -17,33 +23,36 @@ def main():
     )
     client = InfluxDBClient(host=INFLUX_IP, port=INFLUX_PORT, database='streambench-metrics')
 
-    for msg in consumer:
-        points = list()
+    try:
+        for msg in consumer:
+            points = list()
 
-        header = msg.value['header']
-        metrics = msg.value['metrics']
+            header = msg.value['header']
+            metrics = msg.value['metrics']
 
-        timestamp = dt.datetime.fromtimestamp(header['time']/1000).isoformat() + 'Z'
-        header.pop('time')
+            timestamp = dt.datetime.fromtimestamp(header['time']/1000).isoformat() + 'Z'
+            header.pop('time')
 
-        for class_, kv_pairs in metrics.items():
-            point = {
-                'measurement': class_,
-                'tags': header,
-                'time': timestamp,
-                'fields': {}
-            }
+            for class_, kv_pairs in metrics.items():
+                point = {
+                    'measurement': class_,
+                    'tags': header,
+                    'time': timestamp,
+                    'fields': {}
+                }
 
-            for key, value in kv_pairs.items():
-                if isinstance(value, Number):
-                    point['fields'][key] = value
+                for key, value in kv_pairs.items():
+                    if isinstance(value, Number):
+                        point['fields'][key] = value
 
-            # pprint(point)
-            if len(point['fields']) > 0:
-                points.append(point)
+                # pprint(point)
+                if len(point['fields']) > 0:
+                    points.append(point)
 
-        client.write_points(points)
-        print('Sent timestamp={}'.format(timestamp))
+            client.write_points(points)
+            print('Sent timestamp={}'.format(timestamp))
+    except KeyboardInterrupt:
+        print("Done!")
 
 if __name__ == '__main__':
     main()
